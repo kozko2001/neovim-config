c45989ba111b4d4f730032cd62ae7467332c6237
Jordi Coscolla <kozko2001@gmail.com>
2022-04-14 17:33:02 +0200

    init

---
+++ lua/user/colorscheme.lua
@@ -0,0 +1,33 @@
+local status_ok, github = pcall(require, "github-theme")
+if not status_ok then
+  vim.notify("could not load github theme")
+  return
+end
+
+github.setup({
+  theme_style = "light",
+  function_style = "italic",
+  sidebars = {"qf", "vista_kind", "terminal", "packer"},
+
+  -- Change the "hint" color to the "orange" color, and make the "error" color bright red
+  colors = {hint = "orange", error = "#ff0000"},
+
+  -- Overwrite the highlight groups
+  overrides = function(c)
+    return {
+      htmlTag = {fg = c.red, bg = "#282c34", sp = c.hint, style = "underline"},
+      DiagnosticHint = {link = "LspDiagnosticsDefaultHint"},
+      -- this will remove the highlight groups
+      TSField = {},
+    }
+  end
+})
+
+
+--local colorscheme = "github_light" -- here the colorscheme
+
+-- local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
+--if not status_ok then
+--  vim.notify("colorscheme " .. colorscheme .. " not found!")
+--  return
+--end
